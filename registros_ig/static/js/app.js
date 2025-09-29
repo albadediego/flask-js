149afab74e60f1f9748eece42e54d881f994035b
Alba de Diego <albadediegocorrea@gmail.com>
2025-09-29 21:12:05 +0200

    peticion http get

--- registros_ig/static/js/app.js
+++ registros_ig/static/js/app.js
@@ -1,4 +1,15 @@
 console.log("Funciona link a app.js")
+let peticion_movimientos = new XMLHttpRequest() //creo un objeto de tipo XMLHttpRequest
+
+function peticion_movimientos_handler(){
+    if(this.readyState === 4){ //para verificar si es una peticion http
+        if(this.status === 200){ //para saber si el estado de codigo es el correcto
+            alert(this.responseText);
+        }else{
+            alert("Se ha producido un error en la consulta http")
+        }
+    }
+}
 
 function viewForm(){
     document.getElementById("movements_detail").style.display="block";
@@ -14,4 +25,9 @@ window.onload = function(){
 
     let cerrar = document.getElementById("btn_cerrar");
     cerrar.addEventListener("click", hideForm)
+
+    peticion_movimientos.open("GET", "http://localhost:5000/api/v1.0/all");
+    peticion_movimientos.onload = peticion_movimientos_handler
+    peticion_movimientos.onerror = function(){alert("No se ha podido completar la peticion de movimientos")};
+    peticion_movimientos.send();
 }
