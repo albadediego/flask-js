54379e1efc6ad9dcb91562f853f7695e95892960
Alba de Diego <albadediegocorrea@gmail.com>
2025-09-29 20:56:33 +0200

    funcion js para mostrar y ocultar formulario

--- registros_ig/static/js/app.js
+++ registros_ig/static/js/app.js
@@ -1,1 +1,17 @@
 console.log("Funciona link a app.js")
+
+function viewForm(){
+    document.getElementById("movements_detail").style.display="block";
+}
+
+function hideForm(){
+    document.getElementById("movements_detail").style.display="none";
+}
+
+window.onload = function(){
+    let nuevo = document.getElementById("btn_crear");
+    nuevo.addEventListener("click", viewForm)
+
+    let cerrar = document.getElementById("btn_cerrar");
+    cerrar.addEventListener("click", hideForm)
+}
